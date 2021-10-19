d1d5163f842fb889f101fcf07c3fa40829b0b197
t-k- <clock126@126.com>
2021-10-19 12:25:40 -0400

    update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
 # ensure queryIDs in trec run files are only numbers!
 rm -rf fusion_output/ && tools/sweep_polyfuse.py --depth 1000 runs/*
+
+QRELS=qrels.ntcir12-math-browsing-concrete.txt
+for run in $(ls fusion_output); do
+	echo $run
+	trec_eval $QRELS -m bpref -l1 fusion_output/$run
+	trec_eval $QRELS -m bpref -l3 fusion_output/$run
+done

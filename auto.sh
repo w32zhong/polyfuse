96adba34c24b3bb10f07c634806622043c8d718a
t-k- <clock126@126.com>
2022-11-17 22:29:53 -0500

    auto.sh supports arqmath-3

--- auto.sh
+++ auto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ### config ###
-PYA0=/store2/scratch/w32zhong/math-dense-retrievers.emnlp/code/pya0
-QREL=qrels.arqmath-2021-task1-official.txt
-#QREL=qrels.ntcir12-math-browsing.txt
+PYA0=/store2/scratch/w32zhong/math-dense-retrievers.verynew/code/pya0
+QREL=qrels.arqmath-2022-task1-or-task3-origin.txt
+TASK=arqmath3
 ###   END  ###
 
 # ensure queryIDs in trec run files are only numbers!
@@ -16,20 +16,16 @@ rm -f ./qrels.*
 cp $PYA0/topics-and-qrels/$QREL .
 sed -i $QREL -e 's/^A\.//g' -e 's/^NTCIR12-MathWiki-//g'
 
-if [[ $QREL =~ "arqmath" ]]; then
-    ##### Quick and unofficial evaluation ###
-    #for run in $(ls fusion_output); do
-    #    ndcg=$(trec_eval $QREL fusion_output/$run -J -m ndcg | awk '{print $3}')
-    #    map=$(trec_eval $QREL fusion_output/$run -l2 -J -m map | awk '{print $3}')
-    #    p10=$(trec_eval $QREL fusion_output/$run -l2 -J -m P.10 | awk '{print $3}')
-    #    bpref=$(trec_eval $QREL fusion_output/$run -l2 -m bpref | awk '{print $3}')
-    #    echo $run $ndcg $map $p10 $bpref
-    #done
-
+if [[ $TASK =~ "arqmath2" ]]; then
     #### Official evaluation, but costly ###
     $PYA0/eval-arqmath2-task1/preprocess.sh cleanup
     $PYA0/eval-arqmath2-task1/preprocess.sh ./fusion_output/*
-    $PYA0/eval-arqmath2-task1/eval.sh --qrels=$QREL
+    $PYA0/eval-arqmath2-task1/eval.sh --qrels=$QREL --nojudge
+elif [[ $TASK =~ "arqmath3" ]]; then
+    #### Official evaluation, but costly ###
+    $PYA0/eval-arqmath3/task1/preprocess.sh cleanup
+    $PYA0/eval-arqmath3/task1/preprocess.sh ./fusion_output/*
+    $PYA0/eval-arqmath3/task1/eval.sh --qrels=$QREL --nojudge
 else
     for run in $(ls fusion_output); do
         bpref_full=$(trec_eval $QREL fusion_output/$run -l3 -m bpref | awk '{print $3}')
